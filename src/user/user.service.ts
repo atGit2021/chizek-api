db67adc4bc1ebb9f081a7611e5179e048df66ad8
atGit2021 <a.c.turner.gm@gmail.com>
2025-01-15 19:53:23 -0600

    User repository refactor with GraphQL CRUD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
   create(createUserInput: CreateUserInput) {
     return `This action adds a new user, ${createUserInput}`;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.userRepository.find({});
   }
 
   findOne(id: number) {
