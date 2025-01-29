f8fd6dc78297f3760eccb007a80f554bb213889b
atGit2021 <a.c.turner.gm@gmail.com>
2025-01-29 10:04:54 -0600

    Error handling for create user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UserRepository } from './user.repository';
+import { UserInputError } from '@nestjs/apollo';
 
 @Injectable()
 export class UserService {
@@ -13,10 +14,19 @@ export class UserService {
   }
 
   async create(createUserInput: CreateUserInput) {
-    return this.userRepository.create({
-      ...createUserInput,
-      password: await this.hashPassword(createUserInput.password),
-    });
+    try {
+      return await this.userRepository.create({
+        ...createUserInput,
+        password: await this.hashPassword(createUserInput.password),
+      });
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new UserInputError('Email already exists', {
+          extensions: { invalidArgs: ['email'] },
+        });
+      }
+      throw new Error('Database error occurred');
+    }
   }
 
   async findAll() {
