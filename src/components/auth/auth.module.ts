738f2773ca24bab80a69237bc26c5adafd254333
atGit2021 <a.c.turner.gm@gmail.com>
2025-01-30 18:33:41 -0600

    Setup auth controller and local auth guard

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UserModule } from 'src/components/user/user.module';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UserModule],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
