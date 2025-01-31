638a7e8b65ed200656a8a7951f7dbc1595af6d23
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-01-31 17:57:25 -0600

    Added jwt strategy guards and decorators for authentication (#11)

--- src/components/user/user.resolver.ts
+++ src/components/user/user.resolver.ts
@@ -3,6 +3,10 @@ import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { TokenPayload } from '../auth/token-payload.interface';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -14,22 +18,29 @@ export class UserResolver {
   }
 
   @Query(() => [User], { name: 'users' })
+  @UseGuards(GqlAuthGuard)
   findAll() {
     return this.userService.findAll();
   }
 
   @Query(() => User, { name: 'user' })
+  @UseGuards(GqlAuthGuard)
   findOne(@Args('_id') _id: string) {
     return this.userService.findOne(_id);
   }
 
   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.userService.update(updateUserInput._id, updateUserInput);
+  @UseGuards(GqlAuthGuard)
+  updateUser(
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.userService.update(user._id, updateUserInput);
   }
 
   @Mutation(() => User)
-  removeUser(@Args('_id') _id: string) {
-    return this.userService.remove(_id);
+  @UseGuards(GqlAuthGuard)
+  removeUser(@CurrentUser() user: TokenPayload) {
+    return this.userService.remove(user._id);
   }
 }
