aae35a2ddb824fafcd407622dda41f88f9e490f8
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-02-05 15:37:20 -0600

    Revised error handling (#12)

--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -1,9 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UserRepository } from './user.repository';
-import { UserInputError } from '@nestjs/apollo';
 
 @Injectable()
 export class UserService {
@@ -20,12 +19,10 @@ export class UserService {
         password: await this.hashPassword(createUserInput.password),
       });
     } catch (error) {
-      if (error.code === 11000) {
-        throw new UserInputError('Email already exists', {
-          extensions: { invalidArgs: ['email'] },
-        });
+      if (error.message.includes('E11000')) {
+        throw new UnprocessableEntityException('Email already exists.');
       }
-      throw new Error('Database error occurred');
+      throw error;
     }
   }
 
