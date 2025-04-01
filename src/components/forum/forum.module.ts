42632ff2a4c58971df954dd1f4a8723a45600bbd
Andre Turner <79720641+atGit2021@users.noreply.github.com>
2025-04-01 11:02:12 -0500

    revised input args and module (#25) (#26)

--- src/components/forum/forum.module.ts
+++ src/components/forum/forum.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ForumService } from './forum.service';
 import { ForumResolver } from './forum.resolver';
 import { ForumRepository } from './forum.repository';
+import { Forum, ForumSchema } from './entities/forum.entity';
 
 @Module({
-  providers: [ForumResolver, ForumService, ForumRepository],
+  imports: [
+    MongooseModule.forFeature([{ name: Forum.name, schema: ForumSchema }]),
+  ],
+  providers: [ForumResolver, ForumService, ForumRepository, Logger],
 })
 export class ForumModule {}
