e6e31f1e8a1a4a215a0a023c57c8c8245d72353f
atGit2021 <a.c.turner.gm@gmail.com>
2025-05-06 17:02:46 -0500

    separated schema into its own document

--- src/components/forum/forum.module.ts
+++ src/components/forum/forum.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ForumService } from './forum.service';
 import { ForumResolver } from './forum.resolver';
 import { ForumRepository } from './forum.repository';
-import { Forum, ForumSchema } from './entities/forum.entity';
 import { MessageModule } from './message/message.module';
+import { ForumDocument, ForumSchema } from './entities/forum.document';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Forum.name, schema: ForumSchema }]),
+    MongooseModule.forFeature([
+      { name: ForumDocument.name, schema: ForumSchema },
+    ]),
     forwardRef(() => MessageModule),
   ],
   providers: [ForumResolver, ForumService, ForumRepository, Logger],
